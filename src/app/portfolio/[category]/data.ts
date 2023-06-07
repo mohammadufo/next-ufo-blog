210764c4007777e843ed8184dab83c4a3bab69a9
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-06-07 19:50:00 +0330

    implement protfolio page

---
+++ src/app/portfolio/[category]/data.ts
@@ -0,0 +1,62 @@
+export const items = {
+  applications: [
+    {
+      id: 1,
+      title: 'Creative Portfolio',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
+      image: '/air1.jpg',
+    },
+    {
+      id: 2,
+      title: 'Minimal Single Product',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
+      image: '/air2.jpg',
+    },
+    {
+      id: 3,
+      title: 'Strong Together Charity',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
+      image: '/air3.jpg',
+    },
+  ],
+  illustrations: [
+    {
+      id: 1,
+      title: 'Creative Portfolio',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
+      image: '/military1.jpg',
+    },
+    {
+      id: 2,
+      title: 'Minimal Single Product',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
+      image: '/military2.jpg',
+    },
+    {
+      id: 3,
+      title: 'Strong Together Charity',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
+      image: '/military3.jpg',
+    },
+  ],
+  websites: [
+    {
+      id: 1,
+      title: 'Creative Portfolio',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
+      image: '/else1.jpg',
+    },
+    {
+      id: 2,
+      title: 'Minimal Single Product',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
+      image: '/else2.jpg',
+    },
+    {
+      id: 3,
+      title: 'Strong Together Charity',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur vel tenetur necessitatibus unde natus perspiciatis, amet cupiditate ducimus possimus, eaque ex autem id nobis eum dolorem. Neque eveniet fugiat tenetur?',
+      image: '/else3.jpg',
+    },
+  ],
+}
