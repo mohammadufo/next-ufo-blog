effc72afbe3e72f17b5330d0b0c167ad46a461ee
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-06-08 10:11:26 +0330

    add portfilio with categoary and blog page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
 
-const BlogPost = () => {
-  return <div>BlogPost</div>
+const BlogPost = async () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>test title</h1>
+          <p className={styles.desc}>test desc</p>
+          <div className={styles.author}>
+            <Image
+              src="/military2.jpg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>user name</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="/air2.jpg" alt="" fill={true} className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>test content</p>
+      </div>
+    </div>
+  )
 }
 
 export default BlogPost
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import styles from './page.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const Blog = () => {
-  return <div>Blog</div>
+const Blog = async () => {
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/military2.jpg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test title</h1>
+          <p className={styles.desc}>test desc</p>
+        </div>
+      </Link>
+    </div>
+  )
 }
 
 export default Blog
